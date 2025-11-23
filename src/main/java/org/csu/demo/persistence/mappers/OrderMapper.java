4d1734d84aad30372f3b666f53f8e0286fdaf139
potterbroom <Xkl136@outlook.com>
2025-11-23 20:34:38 +0800

    删除多于项目

--- src/main/java/org/csu/demo/persistence/mappers/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package org.csu.demo.persistence.mappers;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.csu.demo.domain.Order;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderMapper extends BaseMapper<Order> {
-}
